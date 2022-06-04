fd1e5fa5f8ac598a86cb870839f60872fab0db4f
Olayemi Winner <olayemiwinner@gmail.com>
2022-06-04 11:18:47 -0700

    My commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Description : prints "Programming is like building a multilingual puzzle"
- *
- * Return : Always 0 (Success)
+ * main - main block
+ * Return : 0
  */
 
 int main(void)
