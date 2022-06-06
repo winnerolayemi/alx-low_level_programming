48a421e2f43cb5f4ac716bd3678465350e99d7fa
Olayemi Winner <olayemiwinner@gmail.com>
2022-06-06 03:01:03 -0700

    My commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	i = 97;
+	j = 65;
+
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+
+	while (j < 91)
+	{
+		putchar(j);
+		j++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
