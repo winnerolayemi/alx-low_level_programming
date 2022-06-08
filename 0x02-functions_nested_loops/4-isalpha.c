5ca061ed77733a3217b21386b2afcb6cb44b602d
Olayemi Winner <olayemiwinner@gmail.com>
2022-06-08 14:02:16 -0700

    My commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isalpha - main entry
+ * @c: a character
+ * Return: returns 0 or 1 depending on condition
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+}
