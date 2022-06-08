d14cc5fce3758fd02922b6fb382eafa429e424af
Olayemi Winner <olayemiwinner@gmail.com>
2022-06-08 14:33:11 -0700

    My commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _print_last_digit - prints last digit
+ * @n: passed argument
+ * Return: the last digit
+ */
+int print_last_digit(int n)
+{
+	int x;
+
+	if (n < 0)
+		n = -n;
+	x = n % 10;
+	_putchar(x + '0');
+	return (x);
+}
